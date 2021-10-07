db3c03c91257847452077f43ff47961658f07162
JCh6 <juanch@outlook.com>
2021-10-07 10:00:47 -0500

    add loadmessagefromqueue method

--- src/main/java/com/juanchaparro/springbootawssqs/controller/SQSController.java
+++ src/main/java/com/juanchaparro/springbootawssqs/controller/SQSController.java
@@ -1,8 +1,11 @@
 package com.juanchaparro.springbootawssqs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SQSController {
+
+    Logger logger = LoggerFactory.getLogger(SQSController.class);
 
     @Autowired
     private QueueMessagingTemplate queueMessagingTemplate;
@@ -20,6 +25,11 @@ public class SQSController {
     @GetMapping("/send/{message}")
     public void sendMessageToQueue(@PathVariable String message) {
         queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
+    }
+
+    @SqsListener(value = "${cloud.aws.end-point.uri}")
+    public void loadMessageFromQueue(String message) {
+        logger.info("Message from SQS: {}", message);
     }
 
 }
