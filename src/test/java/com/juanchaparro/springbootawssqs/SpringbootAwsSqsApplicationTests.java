f8e1b2765e26db5d45ccd02d163d41f8f78f2892
JCh6 <juanch@outlook.com>
2021-10-07 08:39:48 -0500

    first commit

---
+++ src/test/java/com/juanchaparro/springbootawssqs/SpringbootAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchaparro.springbootawssqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
