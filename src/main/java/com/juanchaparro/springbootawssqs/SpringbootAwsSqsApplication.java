a5dfccfaa8239fb845074f37b6434d69b1ab79ba
JCh6 <juanch@outlook.com>
2021-10-07 09:28:40 -0500

    fix sqs configuration file

--- src/main/java/com/juanchaparro/springbootawssqs/SpringbootAwsSqsApplication.java
+++ src/main/java/com/juanchaparro/springbootawssqs/SpringbootAwsSqsApplication.java
@@ -2,9 +2,15 @@ package com.juanchaparro.springbootawssqs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+		ContextStackAutoConfiguration.class,
+		ContextInstanceDataAutoConfiguration.class,
+		ContextRegionProviderAutoConfiguration.class
+})
 public class SpringbootAwsSqsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/juanchaparro/springbootawssqs/config/AWSSQSConfig.java
+++ src/main/java/com/juanchaparro/springbootawssqs/config/AWSSQSConfig.java
@@ -2,14 +2,12 @@ package com.juanchaparro.springbootawssqs.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AWSSQSConfig {
@@ -28,19 +26,16 @@ public class AWSSQSConfig {
         return new QueueMessagingTemplate(amazonSQSAsync());
     }
 
-    @Primary
-    @Bean
-    public AmazonSQSAsync amazonSQSAsync() {
+    private AmazonSQSAsync amazonSQSAsync() {
         return AmazonSQSAsyncClientBuilder
                 .standard()
                 .withRegion(awsRegion)
-                //.withCredentials(awsStaticCredentialsProvider())
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                .withCredentials(awsStaticCredentialsProvider())
                 .build();
     }
 
-    /*public AWSStaticCredentialsProvider awsStaticCredentialsProvider() {
+    private AWSStaticCredentialsProvider awsStaticCredentialsProvider() {
         return new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey));
-    }*/
+    }
 
 }
