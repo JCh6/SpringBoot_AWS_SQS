b7163003739e8953017988a20f42d2401373e0f0
JCh6 <juanch@outlook.com>
2021-10-07 09:15:30 -0500

    add sqs controller

--- src/main/java/com/juanchaparro/springbootawssqs/SpringbootAwsSqsApplication.java
+++ src/main/java/com/juanchaparro/springbootawssqs/SpringbootAwsSqsApplication.java
@@ -2,8 +2,9 @@ package com.juanchaparro.springbootawssqs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
 public class SpringbootAwsSqsApplication {
 
 	public static void main(String[] args) {
